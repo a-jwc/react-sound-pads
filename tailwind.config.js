e9e5ecc84e3b5fa635674ed7398a6c313cb490bb
a-jwc <chauajw@gmail.com>
2022-02-23 13:28:07 -0800

    using tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	mode: "jit",
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				pads: {
+					100: "#fff5db",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
